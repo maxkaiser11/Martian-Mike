c8eb515a414ea7f32d0ae8dd0d662af252c301c9
Max Kaiser <max@kaiser.family>
2024-03-11 21:14:53 +0100

    added traps and its logic

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var start_position = $Startposition
+@onready var player = $Player
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
@@ -10,5 +11,11 @@ func _process(delta: float) -> void:
 
 
 func _on_deathzone_body_entered(body: Node2D) -> void:
-	body.velocity = Vector2.ZERO
-	body.global_position = start_position.global_position
+	reset_player()
+	
+func _on_trap_touched_player() -> void:
+	reset_player()
+
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position = start_position.global_position
