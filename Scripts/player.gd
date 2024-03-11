effe24f4ad60eafdab0f5f97e731fcb3c931ea09
Max Kaiser <max@kaiser.family>
2024-03-11 19:31:06 +0100

    added movement logic

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,7 +1,22 @@
 extends CharacterBody2D
 
+@export var gravity = 400
+@export var speed = 125
+@export var jump_force = 200
+
 @onready var animated_sprite = $AnimatedSprite2D
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("move_right"):
-		animated_sprite.play("run")
+func _physics_process(delta: float) -> void:
+	if is_on_floor() == false:
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+		
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = -jump_force
+	
+	# player movement
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	velocity.x = direction * speed
+	move_and_slide()
